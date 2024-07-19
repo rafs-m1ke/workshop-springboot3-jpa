fa0b2c8ee038bc5ee78eb40c0f68534f991fbbd7
Rafael Silva <rafaelmike33@gmail.com>
2024-07-19 19:15:17 -0300

    Changing version to Java 17

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.flexycoders.course;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CourseApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
