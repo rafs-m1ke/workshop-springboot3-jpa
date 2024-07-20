beea3f2b3ccc04b9c0a918e5e70f1f746ad79d96
Rafael Silva <rafaelmike33@gmail.com>
2024-07-20 00:31:38 -0300

    Update Env Variables

--- src/main/java/com/rafsm1ke/api/ApiApplication.java
+++ src/main/java/com/rafsm1ke/api/ApiApplication.java
@@ -2,16 +2,9 @@ package com.rafsm1ke.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 @SpringBootApplication
 public class ApiApplication {
-
-    @Bean(name="entityManagerFactory")
-    public LocalSessionFactoryBean sessionFactory() {
-        return new LocalSessionFactoryBean();
-    }
 
     public static void main(String[] args) {
 
