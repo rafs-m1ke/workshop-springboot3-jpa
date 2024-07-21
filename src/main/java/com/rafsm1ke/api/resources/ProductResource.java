c8ed57096a1a74ddbc7323df02df7876cb10ee22
Rafael Silva <rafaelmike33@gmail.com>
2024-07-21 17:16:40 -0300

    Swagger documentation

--- src/main/java/com/rafsm1ke/api/resources/ProductResource.java
+++ src/main/java/com/rafsm1ke/api/resources/ProductResource.java
@@ -4,6 +4,9 @@ import com.rafsm1ke.api.entities.Category;
 import com.rafsm1ke.api.entities.Product;
 import com.rafsm1ke.api.services.CategoryService;
 import com.rafsm1ke.api.services.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,12 +23,28 @@ public class ProductResource {
     @Autowired
     private ProductService service;
 
+
+    @Operation(summary = "List all products", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Search completed successfully"),
+            @ApiResponse(responseCode = "422", description = "Invalid request data"),
+            @ApiResponse(responseCode = "400", description = "Invalid params"),
+            @ApiResponse(responseCode = "500", description = "Error performing the query")
+    })
     @GetMapping
     public ResponseEntity<List<Product>> findAll() {
         List<Product> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 
+    @Operation(summary = "List a product", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Search completed successfully"),
+            @ApiResponse(responseCode = "422", description = "Invalid request data"),
+            @ApiResponse(responseCode = "400", description = "Invalid params"),
+            @ApiResponse(responseCode = "404", description = "Product not found"),
+            @ApiResponse(responseCode = "500", description = "Error performing the query")
+    })
     @GetMapping(value = "/{id}")
     public ResponseEntity<Product> findById(@PathVariable Long id) {
         Product obj = service.findById(id);
